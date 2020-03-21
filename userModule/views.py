a7d9dabae8ebb5605cee4cc6b6f8b6dfa7bf8f3b
HandsumeDan <danielrgrisham@gmail.com>
2020-03-21 17:46:46 -0400

    update

---
+++ userModule/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html', {'category': category,
+                                                      'categories': categories,
+                                                      'products': products})
