d0bf1580d1f1bc93c8692c603ec1e4d03d8eb902
HandsumeDan <danielrgrisham@gmail.com>
2020-03-21 18:08:52 -0400

    static folder

--- userModule/models.py
+++ userModule/models.py
@@ -1,25 +1,26 @@
 from django.db import models
-from django.urls import reverse
+#from django.urls import reverse
 
 # Create your models here.
 class Kitchen(models.Model):
-    category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
+    #category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    #slug = models.SlugField(max_length=200, db_index=True)
+    #image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='kitchens/', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    #price = models.DecimalField(max_digits=10, decimal_places=2)
+    #stock = models.PositiveIntegerField()
+    #available = models.BooleanField(default=True)
+    #created = models.DateTimeField(auto_now_add=True)
+    #updated = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        ordering = ('-created',)
-        index_together = (('id', 'slug'),)
+    #class Meta:
+     #   ordering = ('-created',)
+     #   index_together = (('id', 'slug'),)
 
-    def __str__(self):
-        return self.name
+    #def __str__(self):
+    #    return self.name
 
-    def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+    #def get_absolute_url(self):
+    #    return reverse('shop:product_detail', args=[self.id, self.slug])
