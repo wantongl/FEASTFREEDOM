c4be2dce20a3f79d070d482ecd07c83c72fa523f
HandsumeDan <danielrgrisham@gmail.com>
2020-03-21 18:46:52 -0400

    new

--- userModule/models.py
+++ userModule/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-#from django.urls import reverse
+from django.urls import reverse
 
 # Create your models here.
 class Kitchen(models.Model):
-    #category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
+    # category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
     #slug = models.SlugField(max_length=200, db_index=True)
     #image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     image = models.ImageField(upload_to='kitchens/', blank=True)
@@ -16,11 +16,11 @@ class Kitchen(models.Model):
     #updated = models.DateTimeField(auto_now=True)
 
     #class Meta:
-     #   ordering = ('-created',)
-     #   index_together = (('id', 'slug'),)
+    #    ordering = ('-created',)
+    #    index_together = (('id', 'slug'),)
 
-    #def __str__(self):
-    #    return self.name
+    def __str__(self):
+        return self.name
 
-    #def get_absolute_url(self):
-    #    return reverse('shop:product_detail', args=[self.id, self.slug])
+    def get_absolute_url(self):
+        return reverse('userModule:kitchen_detail', args=[self.id, self.slug])
--- userModule/views.py
+++ userModule/views.py
@@ -10,4 +10,4 @@ def kitchen_list(request):
     #    category = get_object_or_404(Category, slug=category_slug)
     #    products = products.filter(category=category)
     #return render(request, 'userModule/kitchen_list.html', {'category': category,'categories': categories,'products': products})
-    return render(request, 'userModule/kitchen_list.html','kitchens':k)
+    return render(request, 'userModule/kitchen_list.html',{'kitchens':k})
