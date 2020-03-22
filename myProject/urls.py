7cb404f12ae0f63cd2f3f80fffe9c160c597a2ca
wantongl <wantong.linlin@gmail.com>
2020-03-22 17:56:00 -0400

    fixing github url

--- myProject/urls.py
+++ myProject/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from django.conf.urls import url, include
+from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,6 @@ urlpatterns = [
     re_path(r'^', include(('serviceProviderApp.urls','serviceProviderApp'), namespace='serviceProviderApp')),
     url(r'^cart/', include(('cart.urls','cart'), namespace='cart')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
