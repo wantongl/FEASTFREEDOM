74014be1b038989ccaff9edfed0ffb4112b5ecd7
HandsumeDan <danielrgrisham@gmail.com>
2020-03-22 17:07:35 -0400

    Mar_22_Afternoon_Update

--- serviceProviderApp/views.py
+++ serviceProviderApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import UpdateView
 from serviceProviderApp.models import Kitchen2Register
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class KitchenUpdate(UpdateView):
@@ -8,3 +9,10 @@ class KitchenUpdate(UpdateView):
     fields=['monday','tuesday','wednesday','thursday','friday','saturday','sunday','mondayStartTime','mondayEndTime','tuesdayStartTime','tuesdayEndTime','wednesdayStartTime','wednesdayEndTime','thursdayStartTime','thursdayEndTime','fridayStartTime','fridayEndTime','saturdayStartTime','saturdayEndTime','sundayStartTime','sundayEndTime']
     template_name = 'serviceProviderApp/kitchenUpdate.html'
     success_url = '..'
+
+    def post(self, request, *args, **kwargs):
+        if "cancel" in request.POST:
+            url = self.get_success_url()
+            return HttpResponseRedirect(url)
+        else:
+            return super(KitchenUpdate, self).post(request, *args, **kwargs)
