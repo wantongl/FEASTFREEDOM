f47e12f1f15f3dd3721716d083859f530a61f542
wantongl <wantong.linlin@gmail.com>
2020-03-23 17:15:59 -0400

    adding security questions

--- userModule/forms.py
+++ userModule/forms.py
@@ -2,6 +2,21 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+SECURITY_QUESTIONS = (
+    ('0','What was the house number and street name you lived in as a child?'),
+    ('1','What were the last four digits of your childhood telephone number?'),
+    ('2','What primary school did you attend?'),
+    ('3','In what town or city was your first full time job?'),
+    ('4','In what town or city did you meet your spouse/partner?'),
+    ('5','What is the middle name of your oldest child?'),
+    ('6',"What are the last five digits of your driver's licence number?"),
+    ('7',"What is your grandmother's (on your mother's side) maiden name?"),
+    ('8',"What is your spouse or partner's mother's maiden name?"),
+    ('9','In what town or city did your mother and father meet?'),
+    ('10','What time of the day were you born? (hh:mm)'),
+    ('11','What time of the day was your first child born? (hh:mm)'),
+)
+
 
 class RegularUserCreation(UserCreationForm):
     email = forms.EmailField(required=True)
