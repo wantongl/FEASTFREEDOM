90d5f66793d431dc43ef3aa2b28fc8d227946b96
wantongl <wantong.linlin@gmail.com>
2020-03-23 10:48:00 -0400

    fixed url linking issue

--- serviceProviderApp/urls.py
+++ serviceProviderApp/urls.py
@@ -7,7 +7,7 @@ from userModule.views import createMenuItem
 urlpatterns = [
     re_path(r'^provider/$',views.ProviderRegisterView, name='providersignup'),
     re_path(r'^provider/(?P<pk>\d+)/$',KitchenUpdate.as_view(),name='kitchenUpdate'),
-    url(r'^provider/(?P<id>\d+)/createMenuItem$', createMenuItem.as_view(), name='createMenuItem'),
+    re_path(r'^provider/(?P<id>\d+)/createMenuItem$', createMenuItem.as_view(), name='createMenuItem'),
     #url(r'^$', views.product_list, name='product_list'),
     #url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
     #url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
--- userModule/apps.py
+++ userModule/apps.py
@@ -3,4 +3,3 @@ from django.apps import AppConfig
 
 class UsermoduleConfig(AppConfig):
     name = 'userModule'
-asdfg
