0e0ad5db7b97f5646347a62878d7bb8ae09b27eb
HandsumeDan <danielrgrisham@gmail.com>
2020-03-23 16:43:32 -0400

    cart displayed correctly

--- myApp/views.py
+++ myApp/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from cart.forms import CartAddProductForm
+from cart.cart import Cart
 
 # Create your views here.
 
 def index(request):
-	return render(request,'myApp/base.html', {'Home_Text': "Welcome to FEASTFREEDOM!  Be a Feast Beast!"})
+	#cart_product_form = CartAddProductForm()
+	cart = Cart(request)
+	# return render(request,'shop/product/detail.html',{'product': kitchen,'cart_product_form': cart_product_form})
+	#return render(request, 'userModule/kitchen_detail.html',{'kitchen': kitchen, 'cart_product_form': cart_product_form})
+	return render(request,'myApp/base.html', {'Home_Text': "Welcome to FEASTFREEDOM!  Be a Feast Beast!", 'cart': cart})
