e493c13340c04ff02bea9a88d35fe7bbe3c22bf7
HandsumeDan <danielrgrisham@gmail.com>
2020-03-24 13:56:17 -0400

    ready

--- orders/tasks.py
+++ orders/tasks.py
@@ -9,7 +9,6 @@ def order_created(order_id):
     """
     order = Order.objects.get(id=order_id)
     subject = 'Order nr. {}'.format(order.id)
-    message = 'Dear {},\n\nYou have successfully placed an order. Your order id is {}.'.format(order.first_name,
-                                                                             order.id)
+    message = 'Dear {} {},\n\nYou have successfully placed a ${} order. Your order id is {}.'.format(order.first_name,order.last_name,order.get_total_cost(),order.id)
     mail_sent = send_mail(subject, message, 'projectfeastfreedom@gmail.com', [order.email],fail_silently=False,auth_user='projectfeastfreedom@gmail.com',auth_password='3Y3un3xb')
     return mail_sent
