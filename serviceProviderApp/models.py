0d1a57aae636453f5c2a52d53443341e8e72cb85
wantongl <wantong.linlin@gmail.com>
2020-03-25 15:47:32 -0400

    custom user

--- serviceProviderApp/models.py
+++ serviceProviderApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class menuItem(models.Model):
--- userModule/admin.py
+++ userModule/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-#from userModule.models import Kitchen
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
-#admin.site.register(Kitchen)
+admin.site.register(User, UserAdmin)
