72af6eb8108b5b1b8ec5f2a1fed64d3de586273b
HandsumeDan <danielrgrisham@gmail.com>
2020-03-26 10:35:27 -0400

    userModule can edit menu items

--- userModule/urls.py
+++ userModule/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 from . import views
 from django.urls import reverse
-from userModule.views import createMenuItem
+from userModule.views import updateMenuItem
 
 urlpatterns = [
     re_path(r'^$', views.kitchen_list, name='kitchen_list'),
     re_path(r'^(?P<id>\d+)/$', views.kitchen_detail, name='kitchen_detail'),
-    #re_path(r'^(?P<id>\d+)/createMenuItem$', createMenuItem.as_view(), name='createMenuItem'),
+    re_path(r'^(?P<pk>\d+)/editMenuItem$', updateMenuItem.as_view(), name='editMenuItem'),
     path('signup/',views.signup,name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='userModule/user_login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
