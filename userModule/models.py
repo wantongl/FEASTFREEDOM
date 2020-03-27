e833f00f33929cd30af46d193b4579ba7053ddf4
wantongl <wantong.linlin@gmail.com>
2020-03-27 15:36:07 -0400

    update

--- userModule/models.py
+++ userModule/models.py
@@ -7,8 +7,8 @@ class User(AbstractUser):
     is_provider = models.BooleanField(default=False)
     question1 = models.CharField(blank=False, max_length=30)
     question2 = models.CharField(blank=False, max_length=30)
-    answer1 = models.CharField(blank=False, max_length=30)
-    answer2 = models.CharField(blank=False, max_length=30)
+    answer1 = models.TextField(blank=False, max_length=30)
+    answer2 = models.TextField(blank=False, max_length=30)
 
     def __str__(self):
         return self.username
